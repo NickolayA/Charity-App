54e5aad0329bda5a418e7a481d79d50afd08d590
mikalai <mikalai@wsb-208-72b.local>
2021-10-22 13:08:10 +0300

    GoogleService filed is added.

--- src/сomponents/CardWrapper/index.tsx
+++ src/сomponents/CardWrapper/index.tsx
@@ -1,8 +1,12 @@
+import {Platform} from 'react-native';
 import styled from 'styled-components/native';
 
 export const CardWrapper = styled.View`
   background-color: ${({theme}) => theme.colors.bg.secondary};
-  border-radius: ${({theme}) => theme.sizes[1]};
+  border-radius: ${({theme}) =>
+    Platform.OS === 'ios'
+      ? theme.sizes[1]
+      : parseInt(theme.sizes[1]) - 5 + 'px'};
   margin-bottom: ${({theme}) => theme.spaces[2]};
   border-width: 0.2px;
   border-color: ${({theme}) => theme.colors.border.primary};
